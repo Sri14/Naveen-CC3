4ac75768a2d294bb084ecdcc18c653abb18f03dd
Sankara Tatavarty <sankara.tatavarty@BRI52315L.local>
2022-02-02 23:44:51 +0000

    initial commit

---
+++ cypress/integration/sauceDemo.js
@@ -0,0 +1,32 @@
+/// <reference types= "Cypress" />
+
+describe('Suacedemo page challenge', () => {
+
+    it.only('Log in and add the highes item to basket and check', () => {
+        let price = []
+        let productName;
+        let highest;
+
+        cy.visit('https://www.saucedemo.com');
+        cy.get('#user-name').type('standard_user');
+        cy.get('#password').type('secret_sauce');
+        cy.get('#login-button').click();
+
+        cy.get(`.inventory_item_price`).then(($elm) => {
+            for (let i = 0; i <= $elm.length; i++) {
+                let text = $elm.text();
+                price = text.split('$');
+            }
+            highest = Math.max.apply(0, price);
+            cy.get(`.inventory_item_price`).contains(highest)
+                .parents(`.inventory_item_description`).find(`.inventory_item_name`)
+                .then(($ele) => {
+                    productName = $ele.text();
+                }).then(() => {
+                    cy.get(`.inventory_item_price`).contains(highest).parent().find(`button`).click();
+                    cy.get(`.shopping_cart_link`).click();
+                    cy.get(`.inventory_item_name`).should('contain', productName);
+                });
+        });
+    });
+});
